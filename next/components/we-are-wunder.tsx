382bd9fd1e909f3bfd6b7be681ac5d3306692cd3
Pavel-Kliukin <9213087@mail.ru>
2023-11-22 03:35:58 +0200

    We are Wunder responsive design done

--- next/components/we-are-wunder.tsx
+++ next/components/we-are-wunder.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 const WeAreWunder = () => {
   return (
     <div className="w-screen flex justify-center">
-      <div className="h-[30vh] max-w-screen-2xl flex mt-20 px-16">
-        <div className="w-1/2 text-primary-600 text-heading-lg font-bold">
-          We are Wunder
+      <div className="h-[30vh] max-w-[1664px] mt-20 px-16 lg:flex">
+        <div className="lg:w-1/2">
+          <div className="mb-10 text-primary-600 text-heading-lg font-bold">
+            We are Wunder
+          </div>
         </div>
-        <div className="w-1/2 flex flex-col">
+        <div className="lg:w-1/2 flex flex-col">
           <div className="mb-4 text-steelgray text-md">
             Our mission is to bring people together and shape the way digital services are used in everyday life. We help our clients to improve their digital business, competitiveness and customer experience.
           </div>
