c1cf4478adf536fec2c3dca88ec70ec9fbc1827a
Pavel-Kliukin <9213087@mail.ru>
2023-12-07 19:53:13 +0200

    Latest releases responsive styling done
    Without animation yet

--- next/components/article-teaser.tsx
+++ next/components/article-teaser.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "next-i18next";
 import { absoluteUrl } from "@/lib/drupal/absolute-url";
 import { formatDate } from "@/lib/utils";
 import { ArticleTeaser } from "@/lib/zod/article-teaser";
-/* import ColorfulArrows from "@/styles/icons/colorful-arrows.svg"; */
+import ColorfulArrows from "@/styles/icons/colorful-arrows.svg";
 
 interface ArticleTeaserProps {
   article: ArticleTeaser;
@@ -18,36 +18,27 @@ export function ArticleTeaser({ article, isReversed }: ArticleTeaserProps) {
 
   return (
     <div className="flex justify-center">
-      <Link
-        href={article.path.alias}
-        className="p-4 mb-7 lg:mb-10 xl:max-w-[80%] flex"
-      >
-        <div className="ml-3 mt-5 flex  lg:flex-row">
-            {article.field_image && (
-              <img
-                src={absoluteUrl(article.field_image.uri.url)}
-                alt={article.field_image.resourceIdObjMeta.alt}
-                className={`float-left max-w-[250px] max-h-[250px] h-auto mb-4 mx-4 flex ${isReversed ? "lg:order-last" : ""}`}
-              />
-            )}
-          <div className=" px-4">
-            <h3
-              className={`text-primary-600 mb-2 line-clamp-2 text-heading-xs font-bold ${
-                isReversed ? "md:text-left" : ""
-              }`}
-            >
-              {article.title}
-            </h3>
-            <p>
-              {article.field_excerpt}
-            </p>
-            <div className="flex  items-center mt-4">
-              <p className="text-primary-600">Read More</p>
-              {/*   <ColorfulArrows /> */}
-            </div>
-          </div>
+      <div className="mb-7 mt-5 lg:mb-10 xl:max-w-[80%] max-md:flex max-md:flex-col">
+        {article.field_image && (
+          <img
+            src={absoluteUrl(article.field_image.uri.url)}
+            alt={article.field_image.resourceIdObjMeta.alt}
+            className={`float-left max-w-[250px] max-h-[250px] h-auto mb-4 mx-4 mr-8 ${isReversed ? "md:ml-8 md:mr-4 md:float-right" : ""}`}
+          />
+        )}
+        <div className="px-4">
+          <h3 className="text-primary-600 mb-2 text-heading-xs font-bold">
+            {article.title}
+          </h3>
+          <p>
+            {article.field_excerpt}
+          </p>
+          <Link className="flex items-center mt-4" href={article.path.alias}>
+            <p className="text-primary-600">Read More</p>
+            <ColorfulArrows />
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
