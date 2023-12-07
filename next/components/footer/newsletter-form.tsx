a838722dccccbf693ac3007eeee5575cb1811fa6
janibehm <jani.behml@gmail.com>
2023-12-07 16:23:55 +0200

    working with form and newsletter module

--- next/components/footer/newsletter-form.tsx
+++ next/components/footer/newsletter-form.tsx
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/ui/form";
+import { FormControl, FormField, FormItem, FormLabel } from "@/ui/form";
 
 import { Button } from "@/ui/button";
 import { Checkbox } from "@/ui/checkbox";
@@ -8,7 +8,7 @@ import { Input } from "@/ui/input";
 import Link from "next/link";
 /* import { toast } from "@/ui/use-toast"; */
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import router, { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -40,16 +40,10 @@ export const NewsletterForm = () => {
       data.terms &&
       data.email
     ) {
-      alert("Thank you for subscribing to our newsletter!");
-      /* toast({
-        variant: "success",
-        title: "Thank you for subscribing to our newsletter!",
-      }); */
-
       const response = await fetch(`/api/footer-newsletter`, {
         method: "POST",
         body: JSON.stringify({
-          webform_id: "footer_newsletter",
+          webform_id: "newsletter",
           news: data.news,
           careers: data.careers,
           events: data.events,
@@ -65,14 +59,9 @@ export const NewsletterForm = () => {
         alert("Error!");
       }
     } else {
-        alert("Please, chose at least one option and accept the terms and conditions.");
-     /*  toast({
-        variant: "destructive",
-        title:
-          "Please, chose at least one option and accept the terms and conditions.",
-      });
-    }*/
-  }; 
+      alert("Succes!");
+    }
+  };
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
@@ -86,128 +75,123 @@ export const NewsletterForm = () => {
   });
 
   return (
-    <div className="px-6 flex-col">
-      <h3 className="text-2xl font-bold uppercase tracking-wider py-2">
-        {" "}
-        {t("footer-newsletter-subscribe")}
-      </h3>
-      <p className="max-w-sm text-md">{t("footer-newsletter-intro")}</p>
-      <p className="py-3 text-md">{t("footer-newsletter-interest")}</p>
-      <Form {...form}>
+    <>
+      <div className="ml-12 flex-1 flex flex-col justify-center ">
+        <p>WANT TO HEAR MORE?</p>
+        <p className="font-normal">
+          Our international experts are ready to help you.
+          <br />
+          <span className="underline">Contact us!</span>
+        </p>
+      </div>
+
+      <div className="flex-1">
+        <p className="pb-5 font-bold">Stay up to date with our newsletter</p>
+        <p className="pb-5">"*" indicates required fields</p>
+        <p className="pb-5">I'M INTERESTED IN</p>
+        <div className="pb-5 flex space-x-4 mt-2">
+          <FormField
+            control={form.control}
+            name="news"
+            render={({ field }) => (
+              <FormItem className="flex flex-row py-0 px-3">
+                <FormControl>
+                  <Checkbox
+                    aria-label={t("news")}
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
+                  {t("news")}
+                </FormLabel>
+              </FormItem>
+            )}
+          />
+          {/* Repeat similar structure for "careers" and "events" with their respective names */}
+          <FormField
+            control={form.control}
+            name="news"
+            render={({ field }) => (
+              <FormItem className="flex flex-row py-0 px-3">
+                <FormControl>
+                  <Checkbox
+                    aria-label={t("careers")}
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
+                  {t("careers")}
+                </FormLabel>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="news"
+            render={({ field }) => (
+              <FormItem className="flex flex-row py-0 px-3">
+                <FormControl>
+                  <Checkbox
+                    aria-label={t("events")}
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
+                  {t("events")}
+                </FormLabel>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="pb-5 mt-4">
+          {/* FormField for email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder={t("Enter your email")}
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="pb-5 flex space-x-4 mt-2">
+          {/* Repeat similar structure for "careers" and "events" with their respective names */}
+        </div>
+        <div className="pb-5 mt-4">
+          {/* FormField for email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder={t("Enter your email")}
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        {/* FormField for terms */}
         <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col">
-          <div className="flex flex-row  ">
-            <FormField
-              control={form.control}
-              name="news"
-              render={({ field }) => (
-                <FormItem className="flex flex-row py-0 px-3">
-                  <FormControl>
-                    <Checkbox
-                      aria-label={field.name}
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
-                    News
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="events"
-              render={({ field }) => (
-                <FormItem className="flex flex-row px-3">
-                  <FormControl>
-                    <Checkbox
-                      aria-label={field.name}
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
-                    Events
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="careers"
-              render={({ field }) => (
-                <FormItem className="flex flex-row">
-                  <FormControl>
-                    <Checkbox
-                      aria-label={field.name}
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <FormLabel className="px-2 mt-0 pt-0 flex  text-md">
-                    Careers
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="flex flex-row flex-wrap gap-4 align-middle ">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      className="py-2 my-2 text-steelgray"
-                      type="email"
-                      placeholder="Enter your email"
-                      {...field}
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button
-              variant={"secondary"}
-              className="hover:bg-finnishwinter hover:text-primary-800 my-1 min-w-[150px] max-h-12"
-              size="md"
-              type={"submit"}
-            >
-              {t("form-subscribe")}
-            </Button>
-          </div>
-          <div className="flex flex-row gap-4 pt-4">
-            <FormField
-              control={form.control}
-              name="terms"
-              render={({ field }) => (
-                <FormItem className="flex flex-row items-start">
-                  <FormControl>
-                    <Checkbox
-                      aria-label={field.name}
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                      required
-                    />
-                  </FormControl>
-                  <FormLabel htmlFor="terms" className=" max-w-sm text-md px-2">
-                    {t("footer-newsletter-terms")}{" "}
-                    <Link
-                      href="/privacy-policy"
-                      className="text-mellow font-bold"
-                    >
-                      {" "}
-                      Privacy Policy{" "}
-                    </Link>
-                    .*
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
-          </div>
+          <Button variant="quatriary" className="mt-10 text-white">
+            {t("Subscribe")}
+          </Button>
         </form>
-      </Form>
-    </div>
+      </div>
+    </>
   );
 };
