6153d8faf531827e400b817fa25d520c16618e7e
Pavel-Kliukin <9213087@mail.ru>
2023-12-13 00:37:35 +0200

    Images scale for portfolio cards done

--- next/components/case-teaser.tsx
+++ next/components/case-teaser.tsx
@@ -14,43 +14,24 @@ interface CaseTeaserProps {
 export function CaseTeaser({ client }: CaseTeaserProps) {
   
   return (
-    // <Link
-    // href={client.path.alias}
-    // className="w-80 h-[30rem] p-4 group/card rounded border border-finnishwinter hover:shadow-md"
-    // // className="grid h-full rounded border border-finnishwinter p-4 hover:shadow-md"
-    // >
-    //   <h3 className="mb-2 text-heading-xs text-secondary-900">
-    //     {client.title}
-    //   </h3>
-    //   <div className="w-full h-[80%] overflow-hidden">
-    //     {client.field_image && (
-    //       <Image
-    //       src={absoluteUrl(client.field_image.uri.url)}
-    //       width={600}
-    //       height={600}
-    //       alt={client.field_image.resourceIdObjMeta.alt}
-    //       className="h-full object-cover group-hover/card:scale-110 duration-300"
-    //       />
-    //     )}
-    //   </div>
-    //   <div className="my-4">{client.field_excerpt}</div>
-    // </Link>
-    <div className="w-80 h-[30rem] p-4 group/card rounded border border-finnishwinter hover:shadow-md">
-      <h3 className="mb-2 text-heading-xs text-secondary-900">
-        {client.title}
-      </h3>
-      <div className="w-full h-[80%] overflow-hidden">
-        {client.field_image && (
-          <Image
-          src={absoluteUrl(client.field_image.uri.url)}
-          width={600}
-          height={600}
-          alt={client.field_image.resourceIdObjMeta.alt}
-          className="h-full object-cover group-hover/card:scale-110 duration-300"
-          />
-        )}
+    <Link href={client.path.alias}>
+      <div className="w-100 h-[30rem] p-4 group/card rounded border border-finnishwinter hover:shadow-md">
+        <h3 className="mb-2 text-heading-xs text-secondary-900">
+          {client.title}
+        </h3>
+        <div className="w-full h-[75%] overflow-hidden">
+          {client.field_image && (
+            <Image
+            src={absoluteUrl(client.field_image.uri.url)}
+            width={600}
+            height={600}
+            alt={client.field_image.resourceIdObjMeta.alt}
+            className="h-full object-cover group-hover/card:scale-110 duration-300"
+            />
+          )}
+        </div>
+        <div className="my-4">{client.field_excerpt}</div>
       </div>
-      <div className="my-4">{client.field_excerpt}</div>
-    </div>
+    </Link>
   );
 }
