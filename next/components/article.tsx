618e2f7927c3b9c9a49c88e57171903369f433d3
Pavel-Kliukin <9213087@mail.ru>
2023-12-19 02:15:28 +0200

    Single article styling done

--- next/components/article.tsx
+++ next/components/article.tsx
@@ -17,58 +17,79 @@ export function Article({ article, ...props }: ArticleProps) {
   const router = useRouter();
   return (
     <article {...props}>
-      <HeadingPage>{article.title}</HeadingPage>
-      {article.field_excerpt && (
-        <div className="my-4 text-xl">{article.field_excerpt}</div>
-      )}
-      <div className="mb-4 text-scapaflow">
-        {article.uid?.display_name && (
-          <div className="flex flex-row items-center">
-            {article.uid?.field_user_image &&
+      <div className="w-screen flex justify-center">
+        <div className="w-full max-w-[1664px] mb-20 px-6 sm:px-16 flex flex-col">
+          <div className="grid gap-8 lg:grid-cols-2 w-full mt-20">
+
+            <figure>
               <Image
-                src={absoluteUrl(article.uid?.field_user_image.uri.url)}
-                width={50}
-                height={50}
-                style={{ width: 50, height: 50 }}
-                alt={article.uid?.field_user_image.resourceIdObjMeta.alt}
-                className="border-0 rounded-full object-cover"
+                src={absoluteUrl(article.field_image.uri.url)}
+                width={768}
+                height={480}
+                alt={article.field_image.resourceIdObjMeta.alt}
+                className="mb-10 object-contain"
                 priority
               />
-            }
-            {!article.uid?.field_user_image && 
-              <img src="/assets/carrot.png" alt="wunder" className="h-[50px] w-[50px] border-0 rounded-full" />
-            }
-            <span>
-              {t("posted-by", { author: article.uid?.display_name })} -{" "}
-            </span>
+              {article.field_image.resourceIdObjMeta.title && (
+                <figcaption className="py-2 text-center text-sm text-scapaflow">
+                  {article.field_image.resourceIdObjMeta.title}
+                </figcaption>
+              )}
+            </figure>
+
+            <div>
+              <HeadingPage>{article.title}</HeadingPage>
+              <div className="mt-4 w-full flex flex-wrap">
+                <div className="flex flex-grow">
+                  <div className="mb-4 flex justify-start items-start">
+                    <div className="text-md text-secondary-900">{formatDate(article.created, router.locale)}</div>
+                  </div>
+                </div>
+                <div className="min-w-[40%]">
+                  {article.uid?.display_name && (
+                    <div className="m-2 flex flex-col justify-center items-center">
+                      {article.uid?.field_user_image &&
+                        <Image
+                          src={absoluteUrl(article.uid?.field_user_image.uri.url)}
+                          width={100}
+                          height={100}
+                          alt={article.uid?.field_user_image.resourceIdObjMeta.alt}
+                          className="object-cover h-[100px] w-[100px] border-0 rounded-full"
+                          priority
+                        />
+                      }
+                      {!article.uid?.field_user_image && 
+                        <Image
+                          src="/assets/noPhoto.png"
+                          width={100}
+                          height={100}
+                          alt="no photo"
+                          className="object-cover h-[100px] w-[100px] border-0 rounded-full"
+                          priority
+                        />
+                      }
+                      <div className="mt-2 text-secondary-900">
+                        {t("posted-by", { author: article.uid?.display_name })}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="my-4 text-md md:text-xl">
+                {article.field_excerpt}
+              </div>
+              <div className="mt-8 flex justify-center items-center">
+                {article.body?.processed && (
+                  <FormattedText
+                    className="mt-4 text-md/xl text-scapaflow sm:text-lg"
+                    html={article.body?.processed}
+                  />
+                )}
+              </div>
+            </div>
           </div>
-        )}
-        <span>{formatDate(article.created, router.locale)}</span>
+        </div>
       </div>
-      {article.field_image && (
-        <figure>
-          <Image
-            src={absoluteUrl(article.field_image.uri.url)}
-            width={768}
-            height={480}
-            style={{ width: 768, height: 480 }}
-            alt={article.field_image.resourceIdObjMeta.alt}
-            className="object-cover"
-            priority
-          />
-          {article.field_image.resourceIdObjMeta.title && (
-            <figcaption className="py-2 text-center text-sm text-scapaflow">
-              {article.field_image.resourceIdObjMeta.title}
-            </figcaption>
-          )}
-        </figure>
-      )}
-      {article.body?.processed && (
-        <FormattedText
-          className="mt-4 text-md/xl text-scapaflow sm:text-lg"
-          html={article.body?.processed}
-        />
-      )}
     </article>
   );
 }
