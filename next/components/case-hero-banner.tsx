dfdb23ca2c352f159570140f03e6c41cc8543fcb
janibehm <jani.behml@gmail.com>
2023-12-21 02:47:42 +0200

    add content on individual cases

---
+++ next/components/case-hero-banner.tsx
@@ -0,0 +1,16 @@
+import { overpass } from "@/styles/fonts";
+
+export const HeroBannerCase = () => {
+  return (
+    <div>
+      <div className="mb-14 relative w-full h-[600px]">
+        <img
+          className="w-full h-full object-cover"
+          src="/assets/client-hero.png"
+          alt="client-hero"
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-primary-600 opacity-60"></div>
+      </div>
+    </div>
+  );
+};
