8592a3ff27302212783636847c8763f06788c53f
Pavel-Kliukin <9213087@mail.ru>
2023-12-22 23:17:04 +0200

    Other Media in other paragraphs fixed to be able to do the build version

--- next/components/paragraph--video.tsx
+++ next/components/paragraph--video.tsx
@@ -2,5 +2,10 @@ import { Media } from "@/components/media";
 import { Video } from "@/lib/zod/paragraph";
 
 export function ParagraphVideo({ paragraph }: { paragraph: Video }) {
-  return <Media media={paragraph.field_video} />;
+  return <Media 
+            media={paragraph.field_video} 
+            width={800}
+            height={800}
+            classname={""}
+         />;
 }
